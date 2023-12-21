d4df1b74ffe9a0ef8e950ae2964bff4880675365
shtanriverdi <shtanriverdi@yahoo.com>
2023-12-21 22:57:21 +0300

    Minor Update

--- ders-ödev/ödev-3/App.tsx
+++ ders-ödev/ödev-3/App.tsx
@@ -1,101 +1,57 @@
-import {View, Text} from 'react-native';
+import { View, Text } from 'react-native';
 import React from 'react';
 
 const App = () => {
   return (
     <>
-      <View style={{padding: 25}}>
+      <View style={{ padding: 25 }}>
         <View
           style={{
             flex: 1,
             flexDirection: 'row',
             justifyContent: 'space-between',
           }}>
-          <Text style={{color: 'gray'}}>Friday, 15 Dec</Text>
-          <Text style={{color: 'gray'}}>Search</Text>
+          <Text style={{ color: 'gray' }}>Friday, 15 Dec</Text>
+          <Text style={{ color: 'gray' }}>Search</Text>
         </View>
 
-        <View style={{paddingVertical: 15}}>
-          <Text style={{fontSize: 30, fontWeight: 'bold', paddingBottom: 5}}>
+        <View style={{ paddingVertical: 15 }}>
+          <Text style={{ fontSize: 30, fontWeight: 'bold', paddingBottom: 5 }}>
             Learn
           </Text>
           <Text>Choose part of the body</Text>
         </View>
 
         <View
-          style={{
-            backgroundColor: '#EB7662',
-            borderRadius: 15,
-            padding: 10,
-            marginBottom: 15,
-          }}>
-          <Text
-            style={{
-              fontSize: 20,
-              fontWeight: 'bold',
-              paddingBottom: 5,
-              color: 'white',
-            }}>
+          style={{ backgroundColor: '#EB7662', borderRadius: 15, padding: 15, marginBottom: 15 }}>
+          <Text style={{ fontSize: 16, fontWeight: 'bold', paddingBottom: 5, color: 'white' }}>
             Head & Face
           </Text>
-          <Text style={{color: '#E9F2FF'}}>11 diseases</Text>
+          <Text style={{ color: '#E9F2FF' }}>11 diseases</Text>
         </View>
 
         <View
-          style={{
-            backgroundColor: '#8DC4BB',
-            borderRadius: 15,
-            padding: 10,
-            marginBottom: 15,
-          }}>
-          <Text
-            style={{
-              fontSize: 20,
-              fontWeight: 'bold',
-              paddingBottom: 5,
-              color: 'white',
-            }}>
-            Head & Face
+          style={{ backgroundColor: '#8DC4BB', borderRadius: 15, padding: 15, marginBottom: 15 }}>
+          <Text style={{ fontSize: 16, fontWeight: 'bold', paddingBottom: 5, color: 'white' }}>
+             Back & Neck
           </Text>
-          <Text style={{color: '#E9F2FF'}}>11 diseases</Text>
+          <Text style={{ color: '#E9F2FF' }}>11 diseases</Text>
         </View>
 
         <View
-          style={{
-            backgroundColor: '#F2982F',
-            borderRadius: 15,
-            padding: 10,
-            marginBottom: 15,
-          }}>
-          <Text
-            style={{
-              fontSize: 20,
-              fontWeight: 'bold',
-              paddingBottom: 5,
-              color: 'white',
-            }}>
-            Head & Face
+          style={{ backgroundColor: '#F2982F', borderRadius: 15, padding: 15, marginBottom: 15 }}>
+          <Text style={{ fontSize: 16, fontWeight: 'bold', paddingBottom: 5, color: 'white' }}>
+           Elbow & Shoulders
           </Text>
-          <Text style={{color: '#E9F2FF'}}>11 diseases</Text>
+          <Text style={{ color: '#E9F2FF' }}>11 diseases</Text>
         </View>
 
         <View
-          style={{
-            backgroundColor: '#327389',
-            borderRadius: 15,
-            padding: 10,
-            marginBottom: 15,
-          }}>
-          <Text
-            style={{
-              fontSize: 20,
-              fontWeight: 'bold',
-              paddingBottom: 5,
-              color: 'white',
-            }}>
-            Head & Face
+          style={{ backgroundColor: '#327389', borderRadius: 15, padding: 15, marginBottom: 15 }}>
+          <Text style={{ fontSize: 16, fontWeight: 'bold', paddingBottom: 5, color: 'white' }}>
+            Hand & Arm
           </Text>
-          <Text style={{color: '#E9F2FF'}}>11 diseases</Text>
+          <Text style={{ color: '#E9F2FF' }}>11 diseases</Text>
         </View>
       </View>
     </>
